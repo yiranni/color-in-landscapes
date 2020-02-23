e2606a1da5bf5997c57c662ea7005eacb17bf64e
Yiran Ni <yiran@yirans-mbp.myfiosgateway.com>
2020-02-23 14:29:30 -0500

    change img path

--- assets/js/bubble.js
+++ assets/js/bubble.js
@@ -9,15 +9,15 @@ const margin = {
     height = 300 - margin.top - margin.bottom;
 
 
-const svg = d3.select('#bubble')
-    .append('div')
-    .classed('svg-contatiner', true)
-    .append('svg')
-    .attr('preserveAspectRatio', 'xMinYMin meet')
-    .attr('viewBox', `0 0 600, 400`)
-    .attr('id', 'svg-content-responsive')
+const svg = d3.select("#bubble")
+    .append("div")
+    .classed("svg-contatiner", true)
+    .append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", `0 0 600, 400`)
+    .attr("id", "svg-content-responsive")
 
-const bubbleChart = d3.select('#svg-content-responsive')
+const bubbleChart = d3.select("#svg-content-responsive")
 
 const x = d3.scaleLinear()
 
@@ -30,6 +30,8 @@ Promise.all([
 ]).then(function (files) {
 
     const final = files[0];
+
+    const defs = svg.append("defs");
 
     x.domain([d3.min(final, function (d) {
             return d.date
@@ -61,47 +63,66 @@ Promise.all([
         .tickPadding(20);
 
 
-    bubbleChart.append('g')
-        .attr('class', 'xAxis')
-        .attr('transform', `translate(${margin.left}, ${height + margin.top})`)
+    bubbleChart.append("g")
+        .attr("class", "xAxis")
+        .attr("transform", `translate(${margin.left}, ${height + margin.top})`)
         .call(xAxis)
-    bubbleChart.append('g')
-        .attr('class', 'yAxis')
+    bubbleChart.append("g")
+        .attr("class", "yAxis")
         .call(yAxis)
-        .attr('transform', `translate(${margin.left},${margin.top})`)
+        .attr("transform", `translate(${margin.left},${margin.top})`)
 
+    defs.selectAll(".painting-pattern")
+        .data(final)
+        .enter().append("pattern")
+        .attr("class", "painting-pattern")
+        .attr("id", function (d) {
+            return d.objectID
+        })
+        .attr("height", "100%")
+        .attr("width", "100%")
+        .attr("patternContentUnits", "objectBoundingBox")
+        .append("image")
+        .attr("height", 1)
+        .attr("width", 1)
+        .attr("preserveAspectRatio", "none")
+        .attr("xmlns:xlinks", "http://www.w3.org/1999/xlink")
+        .attr("xlink:href", function (d) {
+            return "../../src/img/" + d.objectID + ".jpg";
+        })
 
-    bubbleChart.append('g')
-        .selectAll('dot')
+    bubbleChart.append("g")
+        .selectAll("dot")
         .data(final)
         .enter()
-        .append('circle')
-        .attr('cx', function (d, i) {
+        .append("circle")
+        .attr("cx", function (d, i) {
             return x(d.date)
         })
-        .attr('cy', function (d) {
+        .attr("cy", function (d) {
             return y(d.culture)
         })
-        .attr('r', 7)
-        .style('fill', function (d) {
+        .attr("r", 8)
+        .attr("fill", function (d) {
             return d3.rgb(d.colorValue[0], d.colorValue[1], d.colorValue[2])
         })
         .attr("stroke", "white")
         .style("stroke-width", ".5px")
-        .attr('transform', `translate(${margin.left},${margin.top + 35})`)
+        .attr("transform", `translate(${margin.left},${margin.top + 35})`)
 
-    d3.selectAll('.domain').remove();
 
-    d3.selectAll('button.btn')
-        .on('click', function (d) {
-            let selectedOption = d3.select(this).property('value')
+    d3.selectAll(".domain").remove();
+
+    d3.selectAll("button.btn")
+        .on("click", function (d) {
+            let selectedOption = d3.select(this).property("value")
             console.log(selectedOption);
             updateChart(selectedOption);
         })
 
     function updateChart(selected) {
-        d3.selectAll('text').remove();
-        d3.selectAll('circle').remove();
+        d3.selectAll("text").remove();
+        d3.selectAll("circle").remove();
         const nested = d3.nest()
             .key(function (d) {
                 console.log(d[selected])
@@ -123,40 +144,40 @@ Promise.all([
         }))
 
 
-        bubbleChart.append('g')
-            .attr('class', 'xAxis')
-            .attr('transform', `translate(${margin.left}, ${height + margin.top})`)
+        bubbleChart.append("g")
+            .attr("class", "xAxis")
+            .attr("transform", `translate(${margin.left}, ${height + margin.top})`)
             .call(xAxis)
-        bubbleChart.append('g')
-            .attr('class', 'yAxis')
+        bubbleChart.append("g")
+            .attr("class", "yAxis")
             .call(yAxis)
-            .attr('transform', `translate(${margin.left},${margin.top})`)
+            .attr("transform", `translate(${margin.left},${margin.top})`)
 
-            bubbleChart.append('g')
-            .selectAll('dot')
+        bubbleChart.append("g")
+            .selectAll("dot")
             .data(final)
             .enter()
-            .append('circle')
-            .attr('cx', function (d, i) {
+            .append("circle")
+            .attr("cx", function (d, i) {
                 return x(d.date)
             })
-            .attr('cy', function (d) {
+            .attr("cy", function (d) {
                 return y(d[selected])
             })
-            .attr('r', 7)
-            .style('fill', function (d) {
+            .attr("r", 8)
+            .style("fill", function (d) {
                 return d3.rgb(d.colorValue[0], d.colorValue[1], d.colorValue[2])
             })
             .attr("stroke", "white")
             .style("stroke-width", ".5px")
-            .attr('transform', `translate(${margin.left},${margin.top + 35})`)
-    
-        d3.selectAll('.domain').remove();
+            .attr("transform", `translate(${margin.left},${margin.top + 35})`)
+
+        d3.selectAll(".domain").remove();
     }
 
-    bubbleChart.append('text')
-        .text('A.D.')
-        .attr('transform', `translate(${width + margin.left + 18}, ${height + margin.top + 10})`)
+    bubbleChart.append("text")
+        .text("A.D.")
+        .attr("transform", `translate(${width + margin.left + 18}, ${height + margin.top + 10})`)
 }).catch(function (err) {
     // handle error here
 })
